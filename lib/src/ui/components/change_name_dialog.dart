377bcbf6cce8e5ac394ba411ac7d73b60a8f9956
Juan Latorre <juanlatorreharcha@gmail.com>
2020-02-19 21:02:53 -0300

    Add name is empty validation

--- lib/src/ui/components/change_name_dialog.dart
+++ lib/src/ui/components/change_name_dialog.dart
@@ -5,6 +5,7 @@ import 'package:hive/hive.dart';
 void changeNameDialog(BuildContext context, Box<Brew> box, int index) {
   TextEditingController _textController = TextEditingController();
   _textController.text = box.getAt(index).name;
+  final _formKey = GlobalKey<FormState>();
 
   showDialog(
     context: context,
@@ -13,12 +14,24 @@ void changeNameDialog(BuildContext context, Box<Brew> box, int index) {
       content: SingleChildScrollView(
         child: ListBody(
           children: <Widget>[
-            TextField(
-              autocorrect: false,
-              autofocus: true,
-              controller: _textController,
-              style: TextStyle(
-                fontSize: 18,
+            Form(
+              key: _formKey,
+              child: TextFormField(
+                autocorrect: false,
+                autofocus: true,
+                controller: _textController,
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+                decoration: InputDecoration(
+                  hintText: 'Choose a new name',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Name cannot be empty';
+                  }
+                  return null;
+                }
               ),
             ),
           ],
@@ -34,9 +47,11 @@ void changeNameDialog(BuildContext context, Box<Brew> box, int index) {
         FlatButton(
           child: Text('Ok'),
           onPressed: () {
-            box.getAt(index).name = _textController.text;
-            box.getAt(index).save();
-            Navigator.of(context).pop();
+            if (_formKey.currentState.validate()) {
+              box.getAt(index).name = _textController.text;
+              box.getAt(index).save();
+              Navigator.of(context).pop();
+            }
           },
         ),
       ],
