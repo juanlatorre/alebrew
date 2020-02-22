a425cb2ba059bec02274358df8fd403deabf87b4
Juan Latorre <juanlatorreharcha@gmail.com>
2020-02-21 22:21:11 -0300

    Add brew number indicator when is over 10

--- lib/src/ui/pages/brew_page.dart
+++ lib/src/ui/pages/brew_page.dart
@@ -17,6 +17,12 @@ class _BrewPageState extends State<BrewPage> {
   TextEditingController _searchController = TextEditingController();
   String searchResult = "";
 
+  String _getNumberOfElementsInBrewery() {
+    int _elementNumber = Hive.box<Brew>("Brewery").values.length;
+
+    return _elementNumber >= 10 ? " (${_elementNumber.toString()})" : "";
+  }
+
   @override
   Widget build(BuildContext context) {
     final provider = Provider.of<BrewProvider>(context);
@@ -24,91 +30,84 @@ class _BrewPageState extends State<BrewPage> {
       color: Colors.white,
       padding: EdgeInsets.only(right: 30, left: 30, top: 50),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Text(
-                "All",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                )
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  FlatButton(
-                    color: Colors.pink[200],
-                    child: Text(
-                      "New",
+                  Text("All" + _getNumberOfElementsInBrewery(),
                       style: TextStyle(
-                        color: Colors.white,
                         fontSize: 20,
                         fontWeight: FontWeight.bold,
-                      )
+                      )),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        FlatButton(
+                            color: Colors.pink[200],
+                            child: Text("New",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                )),
+                            onPressed: () {
+                              provider.updateNavigation("New Brew");
+                            }),
+                      ]),
+                ]),
+            SizedBox(height: 5),
+            Container(
+              height: 40,
+              child: TextField(
+                autofocus: false,
+                autocorrect: false,
+                textAlignVertical: TextAlignVertical.bottom,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.search),
+                  filled: true,
+                  fillColor: Colors.grey[100],
+                  hintText: "Search brew...",
+                  hintStyle: TextStyle(color: Colors.grey[600]),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey,
                     ),
-                    onPressed: () {
-                      provider.updateNavigation("New Brew");
-                    }
-                  ),
-                ]
-              ),
-            ]
-          ),
-          SizedBox(height: 5),
-          Container(
-            height: 40,
-            child: TextField(
-              autofocus: false,
-              autocorrect: false,
-              textAlignVertical: TextAlignVertical.bottom,
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.search),
-                filled: true,
-                fillColor: Colors.grey[100],
-                hintText: "Search brew...",
-                hintStyle: TextStyle(
-                  color: Colors.grey[600]
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.grey,
                   ),
                 ),
+                controller: _searchController,
+                onChanged: (String value) {
+                  setState(() {
+                    searchResult = value;
+                  });
+                },
               ),
-              controller: _searchController,
-              onChanged: (String value) {
-                setState(() {
-                  searchResult = value;
-                });
-              },
             ),
-          ),
-          SizedBox(height: 20),
-          ValueListenableBuilder(
-            valueListenable: Hive.box<Brew>("Brewery").listenable(),
-            builder: (context, Box<Brew> box, _) {
-              return Flexible(
-                child: ListView.separated(
-                  separatorBuilder: (context, index) {
-                    return Divider(height: 1, color: Colors.black);
-                  },
-                  itemCount: box.values.length,
-                  itemBuilder: (context, index) {
-                    return searchResult == null || searchResult == "" ? 
-                    brewListItem(context, box, index) :
-                    box.getAt(index).name.toLowerCase().contains(searchResult) ?
-                    brewListItem(context, box, index) : 
-                    Container();
-                  },
-                ),
-              );
-            }
-          )
-        ]
-      ),
+            SizedBox(height: 20),
+            ValueListenableBuilder(
+                valueListenable: Hive.box<Brew>("Brewery").listenable(),
+                builder: (context, Box<Brew> box, _) {
+                  return Flexible(
+                    child: ListView.separated(
+                      separatorBuilder: (context, index) {
+                        return Divider(height: 1, color: Colors.black);
+                      },
+                      itemCount: box.values.length,
+                      itemBuilder: (context, index) {
+                        return searchResult == null || searchResult == ""
+                            ? brewListItem(context, box, index)
+                            : box
+                                    .getAt(index)
+                                    .name
+                                    .toLowerCase()
+                                    .contains(searchResult)
+                                ? brewListItem(context, box, index)
+                                : Container();
+                      },
+                    ),
+                  );
+                })
+          ]),
     );
   }
 }
