3b452148ba32c24b7414ec5752f4e866e538ba77
Juan Latorre <juanlatorre@protonmail.com>
2020-02-22 12:52:43 -0300

    Add bottom border color to Appbar

--- lib/src/ui/components/appbar.dart
+++ lib/src/ui/components/appbar.dart
@@ -3,7 +3,15 @@ import 'package:flutter/material.dart';
 Widget appBar(String title) {
   return AppBar(
     iconTheme: IconThemeData(color: Colors.black),
-      title: Text(title, style: TextStyle(color: Colors.black)),
-      backgroundColor: Colors.white,
+    title: Text(title, style: TextStyle(color: Colors.black)),
+    backgroundColor: Colors.white,
+    elevation: 0,
+    bottom: PreferredSize(
+      child: Container(
+        color: Colors.grey,
+        height: 1,
+      ),
+      preferredSize: Size.fromHeight(1),
+    ),
   );
 }
