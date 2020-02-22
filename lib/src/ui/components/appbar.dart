2e41c4808075a953bc7a59b4aec336f0bacfe6aa
Juan Latorre <juanlatorre@protonmail.com>
2020-02-22 13:11:44 -0300

    Add support to brew page title

--- lib/src/ui/components/appbar.dart
+++ lib/src/ui/components/appbar.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 
 Widget appBar(String title) {
   return AppBar(
+    centerTitle: true,
     iconTheme: IconThemeData(color: Colors.black),
-    title: Text(title, style: TextStyle(color: Colors.black)),
+    title: Text(
+      title,
+      style: TextStyle(
+        color: Colors.black,
+        fontSize: 16,
+      ),
+    ),
     backgroundColor: Colors.white,
     elevation: 0,
     bottom: PreferredSize(
